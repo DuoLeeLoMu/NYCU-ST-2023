c00196f99211fba2c94d287022bf4f11c6df6aff
YuCheng <125542224+DuoLeeLoMu@users.noreply.github.com>
2023-03-09 16:48:17 +0800

    Update calculator_test.py

--- Lab03/calculator_test.py
+++ Lab03/calculator_test.py
@@ -1,8 +1,8 @@
 import unittest
 from calculator import Calculator
 
-class ApplicationTest(unittest.TestCase):
 
+class ApplicationTest(unittest.TestCase):
 
     def test_add(self):
         param_list = [(1, 2, 3), (2, 3, 5), (3, 4, 7), (4, 5, 9), (5, 6, 11)]
@@ -11,18 +11,18 @@ class ApplicationTest(unittest.TestCase):
             with self.subTest():
                 self.assertEqual(p3, Calculator.add(p1, p2))
 
-        self.assertRaises(TypeError, Calculator.add, 
+        self.assertRaises(TypeError, Calculator.add,
                           exception_case[0], exception_case[1])
 
     def test_divide(self):
-        param_list = [(2, 1, 2), (4, 2, 2), (15, 5, 3), 
+        param_list = [(2, 1, 2), (4, 2, 2), (15, 5, 3),
                       (56, 7, 8), (77, 7, 11)]
         exception_case = (1, 0)
         for p1, p2, p3 in param_list:
             with self.subTest():
                 self.assertEqual(p3, Calculator.divide(p1, p2))
 
-        self.assertRaises(ZeroDivisionError, Calculator.divide, 
+        self.assertRaises(ZeroDivisionError, Calculator.divide,
                           exception_case[0], exception_case[1])
 
     def test_sqrt(self):
@@ -35,7 +35,7 @@ class ApplicationTest(unittest.TestCase):
         self.assertRaises(ValueError, Calculator.sqrt, exception_case)
 
     def test_exp(self):
-        param_list = [(0, 1), (1, 2.718281828459045), (2, 7.38905609893065), 
+        param_list = [(0, 1), (1, 2.718281828459045), (2, 7.38905609893065),
                       (3, 20.085536923187668), (4, 54.598150033144236)]
         exception_case = 1000
         for p1, p2 in param_list:
